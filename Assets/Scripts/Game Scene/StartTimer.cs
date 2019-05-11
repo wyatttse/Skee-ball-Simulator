3dabe158d81bbd4992ee6af33688e8aaf9830aa8
Wyatt Tse <wyatt_tse@yahoo.com>
2019-05-11 19:51:00 +0800

    Made Floor & Broad and Finished Start Timer

---
+++ Assets/Scripts/Game Scene/StartTimer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StartTimer: MonoBehaviour {
+    private void Start() {
+        StartCoroutine(CountTimeCoroutine());
+
+        System.Collections.IEnumerator CountTimeCoroutine() {
+            var startCounter = GetComponent<UnityEngine.UI.Text>();
+            for (var i = 3; i > 0; --i) {
+                startCounter.text = i.ToString();
+                yield return new WaitForSeconds(1f);
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
